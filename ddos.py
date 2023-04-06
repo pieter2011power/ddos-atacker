9fee6b4c1b33d331b86a0a04014d74ab671d62a0
pieter2011power <61342871+pieter2011power@users.noreply.github.com>
2023-04-06 14:48:07 +0200

    Add files via upload

---
+++ ddos.py
@@ -0,0 +1,94 @@
+import os
+import webbrowser
+import time
+import validators
+import md
+from selenium import webdriver
+
+
+def attack():
+    import webbrowser
+    import time
+
+    mainserver  = ('https://serverat.pieterjansen1.repl.co')
+    atserver =  ('https://atserver.pieterjansen1.repl.co')
+
+
+    print('TRUE')
+    time.sleep(3)
+
+    print('CONNECTING TO THE SERVER...')
+    webbrowser.open(mainserver)
+    print('opening script...')
+    time.sleep(5)
+    webbrowser.open(atserver)
+
+    time.sleep(5)
+    print('server connected')
+    time.sleep(3)
+def after():
+    import os
+    import time
+    
+    py = (os.system('py'))
+    oss= ('import os')
+    script = (os.system('cls'))
+    print('CLEANING PC FROM SERVER ATTACK...')
+    time.sleep(3)
+    oss
+    time.sleep(3)
+    script
+    time.sleep(2)
+    exit()
+
+
+
+
+
+
+attacks = ('')
+website = ('')
+
+
+website = input ('website url:')
+
+validation = validators.url(website, public=True)
+if validation:
+  print("TRUE")
+  
+else:
+  print("FALSE URL exit in 10 seconds")
+  time.sleep(10)
+  exit()
+
+print('loading modules')
+
+
+
+time.sleep(10)
+
+os.system('cls')
+
+time.sleep(3)
+
+
+
+
+
+
+attack()
+
+
+
+time.sleep(40)
+
+os.system('cls')
+
+print(website)
+
+print(attacks)
+
+after()
+
+time.sleep(10)
+exit()
